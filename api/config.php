6acc150adda3bdfa5e8ddc830b8073e4cbcebb65
Binte Hawa <hbinte631@gmail.com>
2026-05-31 10:47:23 +0500

    Update config.php

--- api/config.php
+++ api/config.php
@@ -2,12 +2,13 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 
-$host     = "localhost";
-$dbname   = "skillswap";
-$username = "root";
-$password = "";
+$host     = "sql12.freesqldatabase.com";  // your server from email
+$dbname   = "sql12828776";                // your database name
+$username = "sql12828776";                // your username
+$password = "ycVTdBA479";         // your password from email
+$port     = 3306;
 
-$conn = mysqli_connect($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $username, $password, $dbname, $port);
 
 if (!$conn) {
     die(json_encode(['status' => 'error', 'message' => mysqli_connect_error()]));
